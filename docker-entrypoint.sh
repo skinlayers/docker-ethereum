ff597cebea79f3dd95d3a0746f21c263ca3215b8
Gabriel Burkholder <skinlayers@gmail.com>
2017-12-23 05:34:49 -0800

    Fixes comment.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# if command starts with an option, prepend trezarcoind
+# if command starts with an option, prepend geth
 if [ "${1:0:1}" = '-' ]; then
     set -- /usr/bin/geth "$@"
 fi
